cf88b8cc0aad392d230c976b09dc84484565923f
Kumiko Stewart <kumiko.stewart@gibillpay.com>
2025-07-18 07:24:58 +0900

    commit chater 3-6

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,15 +1,23 @@
+import {
+  createOpenAIModel,
+  generateChatResponse,
+} from "../services/ai-service";
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { messages } = body;
 
-  console.log(useRuntimeConfig());
-
   const id = messages.length.toString();
-  const lastMessage = messages[messages.length - 1];
+  const lastMessages = messages[messages.length - 1];
+
+  const openaiApiKey = useRuntimeConfig().openaiApiKey;
+  const openaiModel = createOpenAIModel(openaiApiKey);
+
+  const response = await generateChatResponse(openaiModel, messages);
 
   return {
     id,
     role: "assistant",
-    content: `(server) You said: ${lastMessage.content}`,
+    content: response,
   };
 });
