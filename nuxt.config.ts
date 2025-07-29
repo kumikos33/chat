923577183e7524b54cb66e376c7fa90596d4dc20
Kumiko Stewart <kumiko.stewart@gibillpay.com>
2025-07-30 07:45:13 +0900

    commit after chapter 4-11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: "2024-11-01",
+//   devtools: { enabled: true },
+//   future: {
+//     compatibilityVersion: 4,
+//   },
+
+//   css: ["~/assets/css/main.css"],
+
+//   modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/mdc"],
+
+//   runtimeConfig: {
+//     openaiAPIKey: process.env.OPENAI_API_KEY,
+//   },
+
+//   mdc: {
+//     highlight: {
+//       theme: "material-theme-palenight",
+//       langs: ["html", "markdown", "vue", "typescript", "javascript", "bash"],
+//     },
+//   },
+// });
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -6,18 +29,5 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
 
-  css: ["~/assets/css/main.css"],
-
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/mdc"],
-
-  runtimeConfig: {
-    openaiAPIKey: process.env.OPENAI_API_KEY,
-  },
-
-  mdc: {
-    highlight: {
-      theme: "material-theme-palenight",
-      langs: ["html", "markdown", "vue", "typescript", "javascript", "bash"],
-    },
-  },
+  modules: ["@nuxt/eslint"],
 });
